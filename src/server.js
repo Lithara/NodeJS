12a86754e1a65032f7cf6f5373c9c47fb4cbce87
Lithara Perera <litharaperera2002@gmail.com>
2023-10-06 21:39:03 +0530

    modified

--- src/server.js
+++ src/server.js
@@ -3,7 +3,21 @@ import http from 'node:http'
 import open from 'open'
 
 const interpolate = (html, data) => {
+    // {{ notes }} => data.notes
   return html.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, placeholder) => {
     return data[placeholder] || '';
   });
 }
+
+const formatNotes = (notes) => {
+    return notes.map(note => {
+      return `
+        <div class="note">
+          <p>${note.content}</p>
+          <div class="tags">
+            ${note.tags.map(tag => `<span class="tag">${tag}</span>`).join('')}
+          </div>
+        </div>
+      `
+    }).join('\n')
+  }
